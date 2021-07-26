fbe12382beba14fbc85e409a030a9745ada1e1dc
@GabrielAlonso1 <gabrieljordaoalonso@gmail.com>
2021-07-25 23:04:39 -0300

    Page Header - concluido

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,25 @@ import './App.css';
 import SideMenu from '../components/SideMenu';
 import { CssBaseline, makeStyles } from '@material-ui/core';
 import Header from '../components/Header';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import PageHeader from '../components/PageHeader';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526'
+    },
+    background: {
+      default: '#f4f5fd'
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -14,13 +33,18 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles()
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+        title="Page Header"
+        subTitle="Page description"
+        icon={<PeopleOutlineTwoToneIcon />}
+        />
       </div>
       <CssBaseline/>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,19 @@ import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme =>({
     root: {
         backgroundColor: '#fff'
     },
     searchInput: {
         opacity: '0.6',
-        padding: '0px 8px',
+        padding: `0px ${theme.spacing(1)}px`,
         fontSize: '0.8rem',
         '&:hover': {
             backgroundColor: '#f2f2f2'
         },
         '& .MuiSvgIcon-root': {
-            marginRight:'8px'
+            marginRight: theme.spacing(1)
         }
     },
     btnRoot: {
@@ -26,7 +26,7 @@ const useStyles = makeStyles({
     btnLabel: {
        // backgroundColor: 'red'
     }
-})
+}))
 
 export default function Header() {
 
